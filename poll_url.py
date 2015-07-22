e131aaf4a2e403eaf1dfbed0de907908519cb50f
John Cowie <jcowie@thoughtworks.com>
2015-07-22 13:40:43 +0100

    Default timeout to 60 and interval to 5

--- poll_url.py
+++ poll_url.py
@@ -8,17 +8,17 @@ def url_status(url):
     response = requests.get(url, timeout=1)
     return response.status_code
 
-def test_url(url):
+def test_url(url, timeout=60, interval=5):
     elapsed = 0
-    while(elapsed < 60):
+    while(elapsed < timeout):
         status = url_status(url)
         if status == 200:
             print "Successfully returned 200"
             return sys.exit(0)
         else:
             print "Returned %s" % status
-            time.sleep(1)
-            elapsed = elapsed + 1
+            time.sleep(interval)
+            elapsed = elapsed + interval
     print "Failed to return 200 status"
     return sys.exit(1)
 
